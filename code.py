ec2361c0bb7a1bdc8d2a4361403f70f99fb86d1a
daromanled <da.led@mail.ru>
2017-03-02 08:54:11 +0900

    Update code.py

--- code.py
+++ code.py
@@ -1,32 +1,45 @@
-import codecs
-import json
-def ten_words_from_news(country_news):
-    Words = {}
-    with codecs.open(country_news, encoding="utf-8") as news:
-        for news in json.load(news)['rss']['channel']['item']:
-           for word in news['description'].split():
-               f = True
-               for letter in word:
-                   if letter not in 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm':
-                       f = False
-               if f == True:
-                    if word in Words.keys():
-                        Words[word] += 1
-                    else:
-                        Words[word] = 1
+def create_cook_book():  
+    products = open('product.txt','r')
+    menu = products.readlines()
+    kol = 0
+    dishes = []
+    cook_book = {}
+    
+    while kol < len(menu):  
+        dish = menu[kol][:-1]
+        dishes.append(dish)
+        kol += 1
+        ingridients = int(menu[kol])
+        kol += 1
+        cook_book[dish] = []
+        
+        for ingridient in range(ingridients):
+            s = menu[kol]
+            product_name = menu[kol][:s.find('|')-1]
+            product_kol = int(menu[kol][s.find('|')+2:s.rfind('|')-1])
+            product_measure = menu[kol][s.rfind('|')+2:-1]
+            kol += 1
+            cook_book[dish].append({'ingridient_name' : product_name, 'ingridient_count' : product_kol, 'ingridient_measure' : product_measure})
+    return dishes, cook_book
+            
+def get_shop_list_by_dishes(cook_book, dishes, person_count):
+    shop_list = {}
+    
+    for dish in dishes:
+        for ingridient in cook_book[dish]:
+            new_shop_list_item = dict(ingridient)
+            new_shop_list_item['ingridient_count'] *= person_count
 
-    Words = sorted(Words.items(), key=lambda x:x[1], reverse = True)
-    for top_word in Words[:11]:
-        print(top_word[0])
+            if new_shop_list_item['ingridient_name'] not in shop_list:
+                shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+            else:
+                shop_list[new_shop_list_item['ingridient_name']]['ingridient_count'] += new_shop_list_item['ingridient_count']
+                
+    print('Список покупок') 
+    for shop_list_item in shop_list.values():
+        print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['ingridient_count'], shop_list_item['ingridient_measure']))
 
-country = input('Введите страну, топ слов из новостей которой вам нужен:')
-if country == 'France' or country == 'Франция':
-    ten_words_from_news('newsfr.json')
-elif country == 'Cypr' or country == 'Кипр':
-    ten_words_from_news('newscy.json')
-elif country == 'Italy' or country == 'Италия':
-    ten_words_from_news('newsit.json')
-elif country == 'Africa' or country == 'Африка':
-    ten_words_from_news('newsafr.json')
-else:
-    print('Sorry, we have not got any information about this country')
+person_count = int(input('Введите количество человек: '))
+dishes, cook_book = create_cook_book()
+get_shop_list_by_dishes(cook_book, dishes, person_count)
+
