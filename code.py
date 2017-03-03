f766bac4b472a8f8acd98abab936547ca8a359c9
daromanled <da.led@mail.ru>
2017-03-03 21:22:49 +0900

    Update code.py

--- code.py
+++ code.py
@@ -1,26 +1,26 @@
-def create_cook_book():  
-    products = open('product.txt','r')
-    menu = products.readlines()
-    kol = 0
-    dishes = []
-    cook_book = {}
-    
-    while kol < len(menu):  
-        dish = menu[kol][:-1]
-        dishes.append(dish)
-        kol += 1
-        ingridients = int(menu[kol])
-        kol += 1
-        cook_book[dish] = []
+def create_cook_book(name_of_file):  
+    with open(name_of_file) as products:
+        menu = products.readlines()
+        kolichestvo = 0
+        dishes = []
+        cook_book = {}
         
-        for ingridient in range(ingridients):
-            s = menu[kol]
-            product_name = menu[kol][:s.find('|')-1]
-            product_kol = int(menu[kol][s.find('|')+2:s.rfind('|')-1])
-            product_measure = menu[kol][s.rfind('|')+2:-1]
-            kol += 1
-            cook_book[dish].append({'ingridient_name' : product_name, 'ingridient_count' : product_kol, 'ingridient_measure' : product_measure})
-    return dishes, cook_book
+        while kolichestvo < len(menu):  
+            menu[kolichestvo] = menu[kolichestvo].strip()
+            dish = menu[kolichestvo]
+            dishes.append(dish)
+            kolichestvo += 1
+            ingridients = int(menu[kolichestvo])
+            kolichestvo += 1
+            cook_book[dish] = []
+            
+            for ingridient in range(ingridients):
+                s = menu[kolichestvo]
+                product_name,product_kol,product_measure = menu[kolichestvo].split(' | ')
+                product_kol = int(product_kol)
+                kolichestvo += 1
+                cook_book[dish].append({'ingridient_name' : product_name, 'ingridient_count' : product_kol, 'ingridient_measure' : product_measure})
+        return dishes, cook_book
             
 def get_shop_list_by_dishes(cook_book, dishes, person_count):
     shop_list = {}
@@ -37,9 +37,12 @@ def get_shop_list_by_dishes(cook_book, dishes, person_count):
                 
     print('Список покупок') 
     for shop_list_item in shop_list.values():
-        print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['ingridient_count'], shop_list_item['ingridient_measure']))
+        print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['ingridient_count'], shop_list_item['ingridient_measure'].strip()))
 
 person_count = int(input('Введите количество человек: '))
-dishes, cook_book = create_cook_book()
-get_shop_list_by_dishes(cook_book, dishes, person_count)
+dishes, cook_book = create_cook_book('product.txt')
+print('Введите заказ в одной строке через пробел')
+dishes_that_persons_want = list(input().split())
+get_shop_list_by_dishes(cook_book, dishes_that_persons_want, person_count)
+
 
