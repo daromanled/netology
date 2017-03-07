5be7cc80059f1a89d1b6400d0e44f72b5909a017
daromanled <da.led@mail.ru>
2017-03-07 09:30:24 +0900

    Update code.py

--- code.py
+++ code.py
@@ -1,23 +1,16 @@
 def create_cook_book(name_of_file):  
-    length = sum(1 for string in open(name_of_file, 'r'))
     with open(name_of_file) as products:
-        number = 0
-        dishes = []
         cook_book = {}
-        while number < length:  
-            dish = products.readline().strip()
-            dishes.append(dish)
-            number += 1
+        for line in products: 
+            dish = line.strip()
             ingridients = int(products.readline().strip())
-            number += 1
             cook_book[dish] = []
             
-            for ingridient in range(ingridients):
-                product_name, product_kol, product_measure = (products.readline().strip()).split(' | ')
-                product_kol = int(product_kol)
-                number += 1
-                cook_book[dish].append({'ingridient_name' : product_name, 'ingridient_count' : product_kol, 'ingridient_measure' : product_measure})
-        return dishes, cook_book
+            for _ in range(ingridients):
+                product_name, product_number, product_measure = (products.readline().strip()).split(' | ')
+                product_number = int(product_number)
+                cook_book[dish].append({'ingridient_name' : product_name, 'ingridient_count' : product_number, 'ingridient_measure' : product_measure})
+        return cook_book
             
 def get_shop_list_by_dishes(cook_book, dishes, person_count):
     shop_list = {}
@@ -37,9 +30,10 @@ def get_shop_list_by_dishes(cook_book, dishes, person_count):
         print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['ingridient_count'], shop_list_item['ingridient_measure'].strip()))
 
 person_count = int(input('Введите количество человек: '))
-dishes, cook_book = create_cook_book('product.txt')
+cook_book = create_cook_book('product.txt')
 print('Введите заказ в одной строке через пробел')
 dishes_that_persons_want = input().split()
 get_shop_list_by_dishes(cook_book, dishes_that_persons_want, person_count)
+
 
 
